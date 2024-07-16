f4c7771ed123d8b9a459af09e32d1f9deba975b9
MaximeMalecot <92maximemalecot@gmail.com>
2024-07-16 17:39:46 +0200

    Fonctionnel

--- app/src/composants/pages/accueil/accueil.tsx
+++ app/src/composants/pages/accueil/accueil.tsx
@@ -1,16 +1,32 @@
-import { Grid, Typography } from '@mui/material';
+import React from 'react';
+
+import { Grid, Switch, Typography } from '@mui/material';
 
 import { MOCK_PROFILS } from '../../../unitsTests/mocks/metier';
 import CarteProfil from '../../molecules/carteProfil/carteProfil';
+import PageProfils from '../Page-profils';
+import PageProfil from '../profil/profil';
 
 export default function PageAccueil() {
+    const [shouldAfficherProfils, setShouldAfficherProfils] = React.useState(false);
+
+    let gérerChangement = (event) => {
+        if(shouldAfficherProfils === true){
+            setShouldAfficherProfils(false);
+        } else {
+            setShouldAfficherProfils(true);
+        }
+        return;
+    }
+
     return (
         <Grid data-testid="page-accueil" container direction="column">
-            <Typography variant="h1">Nos profils</Typography>
+            <Typography variant="h1">Acceuil</Typography>
             <Grid container gap={4} mt={4}>
-                {MOCK_PROFILS.map((profil, idx) => (
-                    <CarteProfil key={`${profil.nom} ${profil.prenom}`} profil={profil} idx={idx} />
-                ))}
+                {shouldAfficherProfils && <PageProfils />}
+            </Grid>
+            <Grid container>
+                <Switch checked={shouldAfficherProfils} onChange={gérerChangement} inputProps={{ 'aria-label': 'controlled' }}/>
             </Grid>
         </Grid>
     );
